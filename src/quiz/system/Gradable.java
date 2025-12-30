bc48bfc57aeb5cf31619fc1fe31f8a2b7d8467d7
Buse <Buse@DESKTOP-LVN01PN>
2025-12-30 17:23:41 +0300

    Add abstract Question class and Gradable interface with Javadoc.

--- src/quiz/system/Gradable.java
+++ src/quiz/system/Gradable.java
@@ -1,5 +1,14 @@
-package quizmodel;
+package quiz.system;
 
+/**
+ * This interface defines the contract for any item that can be graded.
+ * It is a core part of the system's "Interface" implementation.   
+ */
 public interface Gradable {
-
+	/**
+	 * Checks if the provided answer is correct according to the specific question.
+	 * @param answer The input string provided by the student. 
+	 *  
+	 *  */
+   boolean checkAnswer(String answer);
 }
---
+++ src/quiz/system/Question.java
@@ -0,0 +1,40 @@
+package quiz.system;
+
+/**
+ * Base class for all types of questions.
+ * Implementents Gradable interface and provides inheritance for subclasses.
+ */
+public abstract class Question implements Gradable {
+	
+/**The question text-protected so subclasses can access it */
+protected String text;
+
+
+
+/**
+ * Constructor for Question.
+ * @param text The text of the question
+ */
+public Question(String text) {
+	this.text=text; 
+}
+
+
+/**
+ * Gets the text of the question.
+ * @return The question text
+ */
+public String getText() {
+	return text;
+}
+
+
+/**
+ * Abstract method to be implemented by MC and TF questions.
+ * This is the heart of Polymorphism.
+ * @param answer The user's input.
+ * @return true if correct, false otherwise.
+ */
+@Override
+public abstract boolean checkAnswer(String answer);
+}
