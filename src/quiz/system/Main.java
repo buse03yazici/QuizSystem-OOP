d2e3c8be08b7064231dd876d03c7889cebdb168a
Buse <Buse@DESKTOP-LVN01PN>
2026-01-04 20:58:15 +0300

    Added Main class to start quiz system.
    This is the starting file of the program. I added the code to get the
    student's name and connect everything so the quiz can run using the
    questions in the text file.

---
+++ src/quiz/system/Main.java
@@ -0,0 +1,35 @@
+package quiz.system;
+
+import java.util.Scanner;
+
+
+/**
+ * The Main class serves as the entry point for the Quiz System.
+ * It handles the initialization of the student, loads the quiz from question.txt 
+ */
+public class Main {
+	
+/** The main method to execute the system.*/
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in); // creating a scanner object to read user input.
+       
+		//prompt the user for name.
+		System.out.println("Welcome to the Quiz System!");
+		System.out.print("Please enter your name: ");
+		String name= scanner.nextLine(); // to read the student's name.
+		
+		Student student= new Student(name);//to initialize the Student object.
+		
+		
+		//to initialize the Quiz object with the source file.
+		Quiz quiz= new Quiz("questions.txt"); 
+		quiz.start();
+		student.setScore(quiz.calculateScore());
+		
+		scanner.close();
+	}
+	
+	
+	
+
+}
